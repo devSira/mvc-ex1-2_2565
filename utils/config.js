cc25dee2114b97622587ea2ba390e01e9b585efd
devSira <63050198@kmitl.ac.th>
2023-04-26 10:31:57 +0700

    config

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 var config={
     database:{
         host:'localhost',
-        user :'root',
-        password:'123456',
+        user :'',
+        password:'',
         database:'mvc_ex1'
     }
 }
